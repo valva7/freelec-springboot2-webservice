da70f224c416712dda91daa9d2549724e1619909
MZ01-KIMTW\MZ01-KIMTW <kimtw@mz.co.kr>
2022-12-27 16:45:25 +0900

    Post API 추가

---
+++ src/test/java/com/study/gleam/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.study.gleam.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public interface PostsRepositoryTest {
+
+    //@Autowired
+    //PostsRepository postsRepository;
+
+    //@After
+    /*public void cleanup(){
+        postsRepository.deleteAll();
+    }*/
+}
---
+++ src/test/java/com/study/gleam/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,18 @@
+package com.study.gleam.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokFuncTest(){
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
